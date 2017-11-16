a9f3d8423201bf413645a1bbdc81da2f9724c53a
Rasmus Höglund <rhoglund@gmail.com>
2017-11-16 15:21:25 +0100

    Add possibility to force using scroll view.

--- src/index.js
+++ src/index.js
@@ -293,7 +293,7 @@ export default class extends Component {
     // related to https://github.com/leecade/react-native-swiper/issues/570
     // contentOffset is not working in react 0.48.x so we need to use scrollTo
     // to emulate offset.
-    if (Platform.OS === 'ios') {
+    if (Platform.OS === 'ios' || this.props.useScrollView) {
       if (this.initialRender && this.state.total > 1) {
         this.scrollView.scrollTo({...offset, animated: false})
         this.initialRender = false;
@@ -462,7 +462,7 @@ export default class extends Component {
     if (state.dir === 'x') x = diff * state.width
     if (state.dir === 'y') y = diff * state.height
 
-    if (Platform.OS !== 'ios') {
+    if (Platform.OS !== 'ios' && !this.props.useScrollView) {
       this.scrollView && this.scrollView[animated ? 'setPage' : 'setPageWithoutAnimation'](diff)
     } else {
       this.scrollView && this.scrollView.scrollTo({ x, y, animated })
@@ -475,7 +475,7 @@ export default class extends Component {
     })
 
     // trigger onScrollEnd manually in android
-    if (!animated || Platform.OS !== 'ios') {
+    if (!animated || (Platform.OS !== 'ios' && !this.props.useScrollView)) {
       setImmediate(() => {
         this.onScrollEnd({
           nativeEvent: {
@@ -621,7 +621,7 @@ export default class extends Component {
   }
 
   renderScrollView = pages => {
-    if (Platform.OS === 'ios') {
+    if (Platform.OS === 'ios' || this.props.useScrollView) {
       return (
         <ScrollView ref={this.refScrollView}
           {...this.props}
